be891247a841f430a6f505e377654aea8863ca62
hazzanolly1 <155883414+hazzanolly1@users.noreply.github.com>
2024-02-11 20:45:19 +0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Request user to input their name 
+Individual_name = input("Enter your name:")
+print("User's Name:", Individual_name)
+# Request user to enter their country of origin
+Persons_country = input("What is your country of origin?:")
+print("User's Country:", Persons_country)
+# Print on another line Enjoy ur day
+print("Git is Awesome!")
+
+
+# In[ ]:
+
+
+
+
